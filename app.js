ba3d78ad101675f7e3878f505f24ebfbd96cd5a8
Arianne Boyd <boydarianne@yahoo.com>
2023-07-20 22:11:38 -0400

    started javascript lab

---
+++ app.js
@@ -0,0 +1,10 @@
+
+
+console.log("Test for connection")
+
+alert("Loving your hair means loving YOU!"
+)
+document.write("<footer>YOU ARE LOVE</footer>");
+
+if (confirm("Ready to learn new styles?")) {document.write("Let's Go!");} else {document.write("Okay! I'll be here when you are ready.");}
+
