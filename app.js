d8dff7ec9de46dcc77d2f6bc4e2c0fc2ca90ef46
Arianne Boyd <boydarianne@yahoo.com>
2023-07-23 23:17:41 -0400

    changed color of header in css

--- app.js
+++ app.js
@@ -2,9 +2,31 @@
 
 console.log("Test for connection")
 
-alert("Loving your hair means loving YOU!"
-)
-document.write("<footer>YOU ARE LOVE</footer>");
+function statement(){
+    
+    alert("Loving your hair means loving YOU!"
+    )
 
-if (confirm("Ready to learn new styles?")) {document.write("Let's Go!");} else {document.write("Okay! I'll be here when you are ready.");}
+    document.write("YOU ARE LOVE");
+}
+
+
+if (confirm("Ready to learn new styles?")) 
+{document.write("Let's Go!");} 
+else {document.write("Okay! I'll be here when you are ready.");}
+
+
+function getname(){
+    
+    let getname = prompt("Let us know who you are!")
+    
+    while (getname ==""){
+        getname = prompt("enter your name")
+    }
+    document.write(getname)
+}
+    
+
+// for(initialization; condition; afterthought){code to be executed}
+
 
